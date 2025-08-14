0668561f09bf64c68cb29cda91ff9ad688e0ed61
Sarah Fatima <chandpatti41@gmail.com>
2025-08-14 11:37:31 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDLw47EiWI4pFPhQd0WuUCSp7ZxC-DzYKs",
+  authDomain: "bsdchat-78e02.firebaseapp.com",
+  projectId: "bsdchat-78e02",
+  storageBucket: "bsdchat-78e02.appspot.com",
+  messagingSenderId: "635105284266",
+  appId: "1:635105284266:web:39a4f381e0e258662c80d3",
+  measurementId: "G-ND6Y7L1PMY"
 };
 
 // Initialize Firebase
